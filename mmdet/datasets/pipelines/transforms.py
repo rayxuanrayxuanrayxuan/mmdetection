f86a9211dc3f19e76b2955707e5033b4bcb9f583
PJLAB\huanghaian <1286304229@qq.com>
2021-11-05 20:52:25 +0800

    replace affine

--- mmdet/datasets/pipelines/transforms.py
+++ mmdet/datasets/pipelines/transforms.py
@@ -2443,6 +2443,19 @@ class MixUp:
         return repr_str
 
 
+def get_aug_params(value, center=0):
+    if isinstance(value, float):
+        return random.uniform(center - value, center + value)
+    elif len(value) == 2:
+        return random.uniform(value[0], value[1])
+    else:
+        raise ValueError(
+            "Affine params should be either a sequence containing two values\
+                          or single float values. Got {}".format(
+                value
+            )
+        )
+
 @PIPELINES.register_module()
 class RandomAffine:
     """Random affine transform data augmentation.
@@ -2506,41 +2519,36 @@ class RandomAffine:
 
     def __call__(self, results):
         img = results['img']
-        height = img.shape[0] + self.border[0] * 2
-        width = img.shape[1] + self.border[1] * 2
 
-        # Rotation
-        rotation_degree = random.uniform(-self.max_rotate_degree,
-                                         self.max_rotate_degree)
-        rotation_matrix = self._get_rotation_matrix(rotation_degree)
+        theight = img.shape[0] + self.border[0] * 2
+        twidth = img.shape[1] + self.border[1] * 2
 
-        # Scaling
-        scaling_ratio = random.uniform(self.scaling_ratio_range[0],
-                                       self.scaling_ratio_range[1])
-        scaling_matrix = self._get_scaling_matrix(scaling_ratio)
+        # Rotation and Scale
+        angle = get_aug_params(self.max_rotate_degree)
+        scale = get_aug_params(self.scaling_ratio_range, center=1.0)
 
+        if scale <= 0.0:
+            raise ValueError("Argument scale should be positive")
+
+        R = cv2.getRotationMatrix2D(angle=angle, center=(0, 0), scale=scale)
+
+        M = np.ones([2, 3])
         # Shear
-        x_degree = random.uniform(-self.max_shear_degree,
-                                  self.max_shear_degree)
-        y_degree = random.uniform(-self.max_shear_degree,
-                                  self.max_shear_degree)
-        shear_matrix = self._get_shear_matrix(x_degree, y_degree)
+        shear_x = math.tan(get_aug_params(self.max_shear_degree) * math.pi / 180)
+        shear_y = math.tan(get_aug_params(self.max_shear_degree) * math.pi / 180)
+
+        M[0] = R[0] + shear_y * R[1]
+        M[1] = R[1] + shear_x * R[0]
 
         # Translation
-        trans_x = random.uniform(-self.max_translate_ratio,
-                                 self.max_translate_ratio) * width
-        trans_y = random.uniform(-self.max_translate_ratio,
-                                 self.max_translate_ratio) * height
-        translate_matrix = self._get_translation_matrix(trans_x, trans_y)
+        translation_x = get_aug_params(self.max_translate_ratio) * twidth  # x translation (pixels)
+        translation_y = get_aug_params(self.max_translate_ratio) * theight  # y translation (pixels)
 
-        warp_matrix = (
-            translate_matrix @ shear_matrix @ rotation_matrix @ scaling_matrix)
+        M[0, 2] = translation_x
+        M[1, 2] = translation_y
 
-        img = cv2.warpPerspective(
-            img,
-            warp_matrix,
-            dsize=(width, height),
-            borderValue=self.border_val)
+        img = cv2.warpAffine(img, M, dsize=(twidth, theight), borderValue=(114, 114, 114))
+
         results['img'] = img
         results['img_shape'] = img.shape
 
@@ -2548,34 +2556,55 @@ class RandomAffine:
             bboxes = results[key]
             num_bboxes = len(bboxes)
             if num_bboxes:
-                # homogeneous coordinates
-                xs = bboxes[:, [0, 0, 2, 2]].reshape(num_bboxes * 4)
-                ys = bboxes[:, [1, 3, 3, 1]].reshape(num_bboxes * 4)
-                ones = np.ones_like(xs)
-                points = np.vstack([xs, ys, ones])
 
-                warp_points = warp_matrix @ points
-                warp_points = warp_points[:2] / warp_points[2]
-                xs = warp_points[0].reshape(num_bboxes, 4)
-                ys = warp_points[1].reshape(num_bboxes, 4)
+                corner_points = np.ones((4 * num_bboxes, 3))
+                corner_points[:, :2] = bboxes[:, [0, 1, 2, 3, 0, 3, 2, 1]].reshape(
+                    4 * num_bboxes, 2
+                )  # x1y1, x2y2, x1y2, x2y1
+                corner_points = corner_points @ M.T  # apply affine transform
+                corner_points = corner_points.reshape(num_bboxes, 8)
 
-                warp_bboxes = np.vstack(
-                    (xs.min(1), ys.min(1), xs.max(1), ys.max(1))).T
+                # create new boxes
+                corner_xs = corner_points[:, 0::2]
+                corner_ys = corner_points[:, 1::2]
+                new_bboxes = np.concatenate(
+                    (corner_xs.min(1), corner_ys.min(1), corner_xs.max(1), corner_ys.max(1))
+                ).reshape(4, num_bboxes).T
 
-                warp_bboxes[:, [0, 2]] = warp_bboxes[:, [0, 2]].clip(0, width)
-                warp_bboxes[:, [1, 3]] = warp_bboxes[:, [1, 3]].clip(0, height)
+                # clip boxes
+                new_bboxes[:, 0::2] = new_bboxes[:, 0::2].clip(0, twidth)
+                new_bboxes[:, 1::2] = new_bboxes[:, 1::2].clip(0, theight)
 
-                if not self.skip_filter_rule:
-                    # filter bboxes
-                    valid_index = self.filter_gt_bboxes(
-                        bboxes * scaling_ratio, warp_bboxes)
-                    results[key] = warp_bboxes[valid_index]
-                    if key in ['gt_bboxes']:
-                        if 'gt_labels' in results:
-                            results['gt_labels'] = results['gt_labels'][
-                                valid_index]
-                else:
-                    results[key] = warp_bboxes
+                results[key] = new_bboxes
+
+                # # homogeneous coordinates
+                # xs = bboxes[:, [0, 0, 2, 2]].reshape(num_bboxes * 4)
+                # ys = bboxes[:, [1, 3, 3, 1]].reshape(num_bboxes * 4)
+                # ones = np.ones_like(xs)
+                # points = np.vstack([xs, ys, ones])
+                #
+                # warp_points = warp_matrix @ points
+                # warp_points = warp_points[:2] / warp_points[2]
+                # xs = warp_points[0].reshape(num_bboxes, 4)
+                # ys = warp_points[1].reshape(num_bboxes, 4)
+                #
+                # warp_bboxes = np.vstack(
+                #     (xs.min(1), ys.min(1), xs.max(1), ys.max(1))).T
+                #
+                # warp_bboxes[:, [0, 2]] = warp_bboxes[:, [0, 2]].clip(0, width)
+                # warp_bboxes[:, [1, 3]] = warp_bboxes[:, [1, 3]].clip(0, height)
+
+                # if not self.skip_filter_rule:
+                #     # filter bboxes
+                #     valid_index = self.filter_gt_bboxes(
+                #         bboxes * scaling_ratio, warp_bboxes)
+                #     results[key] = warp_bboxes[valid_index]
+                #     if key in ['gt_bboxes']:
+                #         if 'gt_labels' in results:
+                #             results['gt_labels'] = results['gt_labels'][
+                #                 valid_index]
+                # else:
+                #     results[key] = warp_bboxes
 
                 if 'gt_masks' in results:
                     raise NotImplementedError(
